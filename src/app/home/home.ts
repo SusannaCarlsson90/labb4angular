823a5fdae69a79a7572907b7e29a127caba191c5
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-05-13 12:28:20 +0200

    metod för sortering

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,11 +1,11 @@
 import { Component, inject, signal, OnInit } from '@angular/core';
 import { Repository } from '../models/repository';
 import { GithubreposService } from '../services/githubrepos';
-import { CommonModule, DatePipe } from '@angular/common'; // Importera DatePipe
+import { CommonModule} from '@angular/common'; 
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, DatePipe],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
@@ -29,4 +29,14 @@ console.table(this.repositories());
       this.error.set("Kunde inte ladda data, försök igen senare");
     }
   }
+  //Metod för att sortera data
+sortData(key: keyof Repository) {  //För varje key i mitt interface:
+  const sorted = [...this.repositories()].sort((a, b) => //Skapar en kopia av listan för att kunna sortera utan att störa originalet och sorterar sedan från a-b
+  {
+    return a[key].toLowerCase() > b[key].toLowerCase() ? 1 : -1; //Jämför värdet a och b om A är före B flytta ner, annars flytta upp
+  });
+  this.repositories.set(sorted); //Uppdaterar signalen så att ändringen syns på skärmen
 }
+}
+
+
